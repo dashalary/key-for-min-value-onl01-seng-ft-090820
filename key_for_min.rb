88de21ed93ba602078acda7be07ad854ab9adbe6
Dasha Lary <dk2800@nyu.edu>
2020-09-11 17:19:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,3 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
   min_value = nil
 if name_hash == {}
