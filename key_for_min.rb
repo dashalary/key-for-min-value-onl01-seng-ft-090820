2e42129ee1dc9c638e7790785670db33868018d6
Dasha Lary <dk2800@nyu.edu>
2020-09-11 17:26:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ def key_for_min_value(name_hash)
 if name_hash == {}
   nil 
 elsif
-name_hash.collect do |key, value| 
+name_hash.each do |key, value| 
   if value < min_value
  min_value = value 
   key_min_value = key
